245e866ad972a4f147bdd3645fcef48a8efa0cc5
MouseMan2 <jeemouseman2@gmail.com>
2020-04-24 16:24:09 -0700

    fixed game to adjust music levels and hint prefs with menu choices

---
+++ Assets/Connect Four/scripts/HintsOff.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintsOff : MonoBehaviour
+{
+    public GameObject GC;
+
+    Preferences pref;
+    ConnectFour.GameController GCScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pref = GC.GetComponent<Preferences>();
+        GCScript = GC.GetComponent<ConnectFour.GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pref.hints == "0")
+            GCScript.NoTutorial();
+        //else if (pref.hints == "1")
+            //GCScript.YesTutorial();
+    }
+}
---
+++ Assets/Connect Four/scripts/UpdateC4Sound.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateC4Sound : MonoBehaviour
+{
+    public GameObject GC;
+    public GameObject music;
+
+    Preferences pref;
+    AudioSource AS;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pref = GC.GetComponent<Preferences>();
+        AS = music.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AS.volume = pref.music;
+    }
+}
